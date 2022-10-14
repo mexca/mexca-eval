ee8876927863af7293d478b38fcceee8ade3dfae
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2022-10-14 13:59:57 +0200

    scripts for creating different types of dataset

---
+++ video/debate/utils.R
@@ -0,0 +1,171 @@
+# parse time in ms
+# convert the column 'begin_frame' from a character to a POSIXct type to handle time operations (e.g., addition, subtractions)
+# returns a column b_frame with the same content of begin_frame but of POSIXct format
+
+parse_time <- function(df){
+  b_frame <- paste0(substr(df$begin_frame, 1, nchar(df$begin_frame)-4), 
+                    gsub(":", ".", substr(df$begin_frame,nchar(df$begin_frame)-3, 
+                                          nchar(df$begin_frame))))
+  b_frame <- as.POSIXct(b_frame, format = "%H:%M:%OS")  
+  return(b_frame)
+}
+
+# compute time duration
+# takes in the begin_time column and computes the difference between row n+1, and row n
+# returns delta_t
+
+compute_time_duration <- function(t1){
+  delta_t <- vector(mode = "numeric", length(t1))
+  
+  for (i in 1:length(delta_t)){
+    delta_t[i] <- as.numeric(difftime(t1[i+1], t1[i])) 
+  }
+  return(delta_t)
+}
+
+# subset_shots
+# subset dataset 'camera_shots' based on number of faces 'n'.
+# the idea is to select the shots which contain clean clips of faces that matches the n number (i.e., without audience, film or groupshots)
+# it returns a dataframe 'faces_selected_filtered' which is the subset of 'camera_shots'.
+
+subset_shots <- function(n = 1, camera_shots) {
+  # not all rows are annotated. We convert NAs to -1 to allow unambiguous subset
+  camera_shots <- camera_shots |> dplyr::mutate_if(is.numeric, tidyr::replace_na, replace = -1)
+  if( n == 1) {
+    faces_selected <- camera_shots[camera_shots$n_faces == 0 | camera_shots$n_faces == 1,]
+    faces_selected_filtered <- faces_selected[faces_selected$groupshot != 1 & faces_selected$audience != 1 & faces_selected$film != 1,]
+    
+  } else if (n > 1 & n <= 3){
+    faces_selected <- camera_shots[camera_shots$n_faces == n,]
+    faces_selected_filtered <- faces_selected[faces_selected$groupshot != 1 & 
+                                                faces_selected$audience != 1 & 
+                                                faces_selected$film != 1,]
+  }
+  faces_selected_filtered$n <- n
+  return(faces_selected_filtered)
+}
+
+# take the smallest n of frames
+smallest_n_frame <- function(df = temp){
+  df |>
+    dplyr::group_by(face_combination) |>
+    dplyr::tally(frames) |>
+    dplyr::filter(n == min(n)) -> min_frame
+  
+  return(min_frame$n)
+}
+
+# select the same number of shots (based on the minimum) and appends a frames 
+balance_dataset_by_frames <- function(df, n_least_frequent_faces = 0){
+  temp <- df |>
+    dplyr::group_by(face_combination) |>
+    dplyr::slice_sample(n = min(n_least_frequent_faces)) # matches the groups for the smalles n of shots
+  
+# -- commented out as I was unable to trim videos based on frame number -- will come back on this later on if necessary
+  # # create empty df  
+  # minimal_clip <- data.frame(matrix(ncol = ncol(temp)+1, nrow =0))
+  # colnames(minimal_clip) <- colnames(temp)
+  # 
+  # # create empty vector for appending frames real number 
+  # frames_column <- vector(mode = 'integer', length = 0)
+  # 
+  # for(x in 1:nrow(temp)){
+  #   n_frame_rows <- as.numeric(temp[x, c("frames")])
+  #   minimal_clip <- rbind(minimal_clip, temp[rep(x, n_frame_rows),])
+  #   frames_column <- append(frames_column, seq_len(n_frame_rows))
+  # }
+  # 
+  # minimal_clip$frames <- frames_column; rm(frames_column) # remove this vector as not necessary anymore
+  
+  
+  #min_frame <- smallest_n_frame(df = temp)
+  
+#  minimal_clip_balanced <- minimal_clip |>
+#    dplyr::group_by(face_combination) |>
+#    dplyr::slice_sample(n = min_frame) # balance the dataset now having the same number of frame (i.e., same time duration)
+  minimal_clip_balanced <- temp
+  minimal_clip_balanced$face_combination <- as.factor(minimal_clip_balanced$face_combination)
+  return(minimal_clip_balanced)  
+}
+
+# count_face_combination
+# count how many times a particular set of faces is present within the dataset
+# returns a table sorted from max to min
+count_face_combination <- function(df){
+  df$face_combination <- paste0(df$face_1, df$face_2, df$face_3)
+  
+  df |>
+    dplyr::count(face_combination, sort = TRUE)
+}
+
+# select_most_frequent_faces
+# returns a list of shots with the most frequent faces displayed
+# write = T saves them in a txt file
+select_most_frequent_faces <- function(df, write = T, list_shots_path = 'list_shots.txt', 
+                                       out_name = 'faces', n_unique_combinations = 1, 
+                                        balanced = F){
+  
+  df$face_combination <- paste0(df$face_1, df$face_2, df$face_3)
+  most_frequent_faces <- count_face_combination(df)[1:n_unique_combinations,1]
+  faces <- df[df$face_combination %in% most_frequent_faces,]
+  
+  n_least_frequent_faces <- min(count_face_combination(df)[1:n_unique_combinations,2])
+  
+  if(balanced == T){ # if we want all faces to be displayed the same amount of time
+    faces <- balance_dataset_by_frames(df = faces, n_least_frequent_faces)
+  }
+  
+  if(write == T){
+    list_shots <- read.table(list_shots_path, header = F)
+    faces$abs_path <- vector(mode = "character", length = nrow(faces))
+    
+    for(x in 1:nrow(faces)){
+      faces$abs_path[x] <- list_shots[grep(paste0(faces$filename[x],".mp4"), list_shots$V1),]
+    }
+    
+    write.table(paste0("file '",unique(faces$abs_path),"'"), file = paste0(out_name, ".txt"), row.names = F, col.names = F, quote = F)
+
+  }
+  
+  return(faces)
+}
+
+# make clip
+make_clip <- function(number_of_speakers = seq_len(3), camera_shots_annotation, contemporaries = T, n_unique_combination = 1){
+  
+  if(contemporaries == T ){
+    if(max(number_of_speakers)>3){
+      stop("Error. number_of_speakers can't be > 3")
+    }
+    
+    for(speaker in number_of_speakers){
+      out_name <- paste0(speaker,"_speaker_contemporary")
+      
+      # make subsets based on the number of speakers
+      subset_n_face <- subset_shots(camera_shots = camera_shots_annotation, n = speaker)
+      # Count frequencies of sets of faces displayed per time point and select most frequent face combination
+      final_subset_faces <- select_most_frequent_faces(df = subset_n_face, write = T, list_shots_path = 'list_shots.txt', out_name, n_unique_combinations = 1, balanced = F)
+      # concatenate the selected shots 
+      system(paste0("ffmpeg -f concat -safe 0 -i ", out_name,".txt -c copy ", out_name,".mp4"))
+      
+    }
+  }
+  
+  if(contemporaries == F ){
+    if(max(n_unique_combination)>10){
+      stop("Error. number_of_speakers can't be > 10")
+    }
+    
+    for(combination in n_unique_combination){
+      out_name <- paste0(combination,"_speaker_total")
+      # make subsets based on the number of speakers
+      subset_n_face <- subset_shots(camera_shots = camera_shots_annotation, n = 1)
+      # Count frequencies of sets of faces displayed per time point and select most frequent face combination
+      final_subset_faces <- select_most_frequent_faces(df = subset_n_face, write = T, list_shots_path = 'list_shots.txt', out_name, n_unique_combinations = combination, balanced = T)
+      # concatenate the selected shots 
+      system(paste0("ffmpeg -f concat -safe 0 -i ", out_name,".txt -c copy ", out_name,".mp4"))
+      
+    }
+  }
+  
+}
