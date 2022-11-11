e38e44d6860ef96f0b6f1a5280079ac9594927cd
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2022-11-11 17:11:39 +0100

    updates on video performance

--- video/debate/utils.R
+++ video/debate/utils.R
@@ -348,4 +348,10 @@ minMatching <- function(reference, model) {
   
 }
 
+data_summary <- function(x) {
+  m <- mean(x)
+  ymin <- m-sd(x)
+  ymax <- m+sd(x)
+  return(c(y=m,ymin=ymin,ymax=ymax))
+}
 
